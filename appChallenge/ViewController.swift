3cb25326a76e91f6c2e043438d86b3bfdfc13d1c
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-12 17:46:48 +0200

    Initial Commit

---
+++ appChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 12/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
