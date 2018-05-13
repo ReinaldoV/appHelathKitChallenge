c2c74b737ef604002bec1ba999365eccb9eea92f
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-13 19:33:04 +0200

    [feat] Made the outlets of the detailview

--- appChallenge/ObjectiveDetail/ViewController/ObjectiveDetailViewController.swift
+++ appChallenge/ObjectiveDetail/ViewController/ObjectiveDetailViewController.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018 adidas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ObjectiveDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var checkImageView: UIImageView!
+    @IBOutlet weak var percentageLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var trophyImageView: UIImageView!
+}
