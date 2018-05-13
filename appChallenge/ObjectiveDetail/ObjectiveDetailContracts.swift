f0fc7e9337c5462ae86c51c7d2ff044b628a2753
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-13 10:26:16 +0200

    [feat] Added the files of the Objectives List and Objectives detail screens

---
+++ appChallenge/ObjectiveDetail/ObjectiveDetailContracts.swift
@@ -0,0 +1,9 @@
+//
+//  ObjectiveDetailContracts.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 13/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+import Foundation
---
+++ appChallenge/ObjectiveDetail/ViewController/ObjectiveDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ObjectiveDetailViewController.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 13/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+import Foundation
