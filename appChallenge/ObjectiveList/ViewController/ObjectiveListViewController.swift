f33d13146cc659daf0be91ab348d24348d12fb0c
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-13 11:25:13 +0200

    [feat] Added the outlets of the objective list view

--- appChallenge/ObjectiveList/ViewController/ObjectiveListViewController.swift
+++ appChallenge/ObjectiveList/ViewController/ObjectiveListViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 adidas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ObjectiveListViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITableView!
+}
+
+extension ObjectiveListViewController: ObjectiveListViewProtocol {
+    func showObjectiveList(objectivesList: [ObjectiveListViewItemModel]) {
+        
+    }
+}
+
+extension ObjectiveListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
