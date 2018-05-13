5ecf3a2eab97d5262d4f3f81cf3ce0fc52532f8a
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-13 10:45:11 +0200

    [feat] Added the models of the views

---
+++ appChallenge/ObjectiveDetail/Models/ObjectiveDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ObjectiveDetailViewModel.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 13/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+struct ObjectiveDetailViewModel {
+    let title: String
+    let description: String
+    let percentage: Int
+    let current: Float
+    let goal: Float
+    let reward: Reward
+}
---
+++ appChallenge/ObjectiveList/Models/ObjectiveListViewItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ObjectiveListViewItemModel.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 13/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+struct ObjectiveListViewItemModel {
+    let title: String
+    let description: String
+    let medal: Medals
+    let isCompleted: Bool
+}
