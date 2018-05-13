b4cc6611a227f628c74b05cc4e6471e30dd82ead
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2018-05-13 11:16:17 +0200

    [feat] Created the cells of the objective list

---
+++ appChallenge/ObjectiveList/ViewController/Cell/ObjectiveListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectiveListCell.swift
+//  appChallenge
+//
+//  Created by Reinaldo Villanueva Javierre on 13/5/18.
+//  Copyright © 2018 adidas. All rights reserved.
+//
+
+import UIKit
+
+class ObjectiveListCell: UITableViewCell {
+    
+    @IBOutlet weak var medalImageView: UIImageView!
+    @IBOutlet weak var checkImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
